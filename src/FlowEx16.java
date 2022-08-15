78abd65b33f158d774895e51648978e08a442019
AeryShin <108650786+AeryShin@users.noreply.github.com>
2022-08-16 02:19:09 +0900

    Add files via upload

---
+++ src/FlowEx16.java
@@ -0,0 +1,10 @@
+public class FlowEx16 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print("*");              // **********을 5줄 출력
+            }
+            System.out.println();
+        }
+    }
+}
